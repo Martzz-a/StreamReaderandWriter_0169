72aabd17eb5d894879f5e04fab89ed489ade042e
Martzz-a <maritzafidelyah@gmail.com>
2026-06-01 19:03:07 +0700

    Menambahkan Blok try untuk Pengujian Error

--- ManualException.cpp
+++ ManualException.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 int main()
 {
-    
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+
+        //throw 0.5; //melemparkan sebuah integer maka
+        throw 5;
+
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+
+    return 0;
 }
